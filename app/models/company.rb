07345d80c5139165ffa21b56ea61d476e517adb8
MarsMwau <marthamumbua16@gmail.com>
2023-05-31 12:45:42 +0300

    Complete Code Challenge

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,8 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def self.oldest_company
+        order(:founding_year).first
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,11 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "Free Subscribtion", company_id: "1", dev_id: "1", value:"20")
+Freebie.create(item_name: "T-Shirt", company_id: "4", dev_id: "2", value:"10")
+Freebie.create(item_name: "Socks", company_id: "3", dev_id: "3", value:"25")
+Freebie.create(item_name: "Shoes", company_id: "2", dev_id: "4", value:"15")
+
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
